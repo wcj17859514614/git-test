de537c9d2cb6a02a6601538bc61a835878dbaaf4
你大爷就是你大爷 <49677281+wcj17859514614@users.noreply.github.com>
2019-04-17 13:07:07 +0800

    Update TestTGit.java

--- src/main/java/com/baizhi/wcj/test/TestTGit.java
+++ src/main/java/com/baizhi/wcj/test/TestTGit.java
@@ -3,5 +3,6 @@ package com.baizhi.wcj.test;
 public class TestTGit {
     public void testGit() {
         System.out.println("Hello World");
+        System.out.Println("this is testGit");
     }
 }
